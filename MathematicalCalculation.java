4d4c85f92e77343ba05a4082bea2a5e78c5a6334
sheetal-salunke <salunkesheetal7@gmail.com>
2024-06-08 22:59:54 +0530

    Mathematical Calculation

--- MathematicalCalculation.java
+++ MathematicalCalculation.java
@@ -2,7 +2,7 @@ public class MathematicalCalculation {
     public static void main(String[] args)
     {
         int input1=10;
-        int input2=6;
+        int input2=-6;
 
  int sum=input1 + input2;
  int diff=input1 - input2;
@@ -12,22 +12,17 @@ public class MathematicalCalculation {
         System.out.println("Difference of two integers: "+ diff);
         System.out.println("Product of two integers: "+ product);
         System.out.println("Average of two integers: "+average);
-        int distance=Math.abs(input1 - input2);
+              int distance=input1 - input2;
         System.out.println("Distance of two integers: "+distance );
 
         if(input1>input2)
         {
             System.out.println("Max Number: "+input1);
+            System.out.println("Min Number: "+input2);
         }
         else {
             System.out.println("Max Number: "+input2);
-        }
-        if(input1<input2)
-        {
             System.out.println("Min Number: "+input1);
-        }
-        else {
-            System.out.println("Min Number: "+input2);
         }
 
     }
