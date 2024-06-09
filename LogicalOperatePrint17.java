52932c2e520fcf23c3f6c84086fe27a46667410a
sheetal-salunke <salunkesheetal7@gmail.com>
2024-06-10 01:43:30 +0530

    Print 17

---
+++ LogicalOperatePrint17.java
@@ -0,0 +1,8 @@
+public class LogicalOperatePrint17 {
+    public static void main(String[] args)
+    {
+        //number 1,2 ,6,7
+        int test=((6-1)*2)+7;
+        System.out.println("Print value 17: "+test);
+    }
+}
