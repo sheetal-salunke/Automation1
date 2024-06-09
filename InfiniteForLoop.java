56041fee077d7fe97584e6518b6df9d491d25459
sheetal-salunke <salunkesheetal7@gmail.com>
2024-06-10 01:16:23 +0530

    Infinite for loop

---
+++ InfiniteForLoop.java
@@ -0,0 +1,16 @@
+public class InfiniteForLoop {
+    public static void main(String[] args)
+    {
+        //Infinite for loop
+        for(;;)
+        {
+            System.out.println("Hello");
+        }
+
+        //Infinite for loop
+        for(int i=0; ;i++)
+        {
+            System.out.print(i+ " ");
+        }
+    }
+}
---
+++ NewForLoop.java
@@ -0,0 +1,15 @@
+public class NewForLoop {
+    public static void main(String[] args)
+    {
+//        for(;;)
+//        {
+//            System.out.println("Hello");
+//        }
+//        for(int i=0;1;i++)
+//        {
+//            System.out.println("Hello");
+//            break;
+//
+//        }
+    }
+}
